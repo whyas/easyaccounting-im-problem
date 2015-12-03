9eb0fa78779fbaf96de2052fd8190f61358ab554
rankun203 <rankun203@gmail.com>
2015-12-03 13:11:10 +0800

    update api doc

---
+++ bin/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+raml2html -i raml/api.raml > index.html ; open -a Google\ Chrome ./index.html & git add . && git commit -m "update api doc"; git push && git push github master && ./bin/update-doc.sh
